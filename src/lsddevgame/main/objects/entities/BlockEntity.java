654368182e99709fdc8a05237fb0ed8f6f08d649
watashiwalds <dangson2124.ld@gmail.com>
2024-04-23 10:34:05 +0700

    remove todo

--- src/lsddevgame/main/objects/entities/BlockEntity.java
+++ src/lsddevgame/main/objects/entities/BlockEntity.java
@@ -25,7 +25,6 @@ public class BlockEntity extends Entity implements Cloneable {
     public BlockEntity clone() {
         try {
             BlockEntity clone = (BlockEntity) super.clone();
-            // TODO: copy mutable state here, so the clone can't change the internals of the original
             return clone;
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
